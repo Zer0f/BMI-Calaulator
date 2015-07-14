275095a9195470a7ddee4219ca8561573db6ca00
ShepherdAlbert <shepherdalbert@ShepherdAlbertdeMacBook-Pro.local>
2015-07-14 16:11:43 +0800

    Initial Commit

---
+++ BMI Calaulator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BMI Calaulator
+//
+//  Created by ShepherdAlbert on 15/7/14.
+//  Copyright (c) 2015年 ShepherdAlbert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BMI Calaulator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BMI Calaulator
+//
+//  Created by ShepherdAlbert on 15/7/14.
+//  Copyright (c) 2015年 ShepherdAlbert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
